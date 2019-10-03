09fe4c437e1a92821d4b7ac5ed3a4a5523ca10f0
Walter-Feng <walterfeng@foxmail.com>
2019-10-03 15:28:59 +0100

    add np_sort_by_column into utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,13 @@
+import numpy as np
+
 flatten = lambda l: [item for sublist in l for item in sublist]
 
 transpose = lambda l: list(map(list, zip(*l)))
 
 def pick_indexed_element(target_list,index):
     return[i[index] for i in target_list]
+
+
+def np_sort_by_column(target_list, index):
+    return target_list[target_list[:, index].argsort()]
+
