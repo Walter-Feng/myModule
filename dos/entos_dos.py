0dc824a022cc2332188e5cc42227d6bec34d2b6a
Walter-Feng <walterfeng@foxmail.com>
2019-10-03 15:28:00 +0100

    modification on entos_dos

--- dos/entos_dos.py
+++ dos/entos_dos.py
@@ -5,14 +5,17 @@ class entos_dos(object):
     def __init__(self,dos_dictionary):
         tot_dos = dos_dictionary['total_density_of_states']
         self.tot_dos = dos.dos(tot_dos['energy_grids'],tot_dos['density'])
-        
-        bands_dos = dos_dictionary['bands_density_of_states']
-        n_bands =  bands_dos['n_bands']
-        self.bands_dos = [dos.dos(bands_dos['band_' + str(i)]['energy_grids'],bands_dos['band_' + str(i)]['density']) for i in range(n_bands)]
 
         proj_dos = dos_dictionary['projected_density_of_states']
-        n_ao =  proj_dos['n_ao']
+        n_ao = proj_dos['n_ao']
+        self.n_ao = n_ao
         self.proj_dos = [dos.dos(proj_dos['orbital_' + str(i)]['energy_grids'],proj_dos['orbital_' + str(i)]['density']) for i in range(n_ao)]
+
+        gp_dos = dos_dictionary['gross_population_density_of_states']
+        n_ao = proj_dos['n_ao']
+        self.gp_dos = [dos.dos(gp_dos['orbital_' + str(i)]['energy_grids'],gp_dos['orbital_' + str(i)]['density']) for i in range(n_ao)]
+
+        self.fermi_energy = dos_dictionary['Fermi_energy']
 
 def file_extract(json_dir):
     with open(json_dir,'r') as f:
