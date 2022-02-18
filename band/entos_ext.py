f8cf065d52153ef3d386d10be1771e80cf5af4e5
Rui Li <rli3@caltech.edu>
2022-02-17 16:22:00 -0800

    slight change to entos band parsing

--- band/entos_ext.py
+++ band/entos_ext.py
@@ -18,15 +18,16 @@ def band_coordinates_list(band_path_dict):
     return coordinates_list
 
 def band_eigenvalues_list(band_path_dict):
-    tot_number = len(band_path_dict['sample_0']['density_re'])
+    tot_number = len(band_path_dict['sample_0']["k_channel_0"]['density_re'])
     n_samples = band_path_dict['n_samples']
 
-    eigenvalues = np.array([band.np_array_fill_math_inf(np.array(band_path_dict['sample_'+str(i)]['eigenvalues']),tot_number) for i in range(n_samples)])
+    eigenvalues = np.array([band.np_array_fill_math_inf(
+        np.array(band_path_dict['sample_'+str(i)]["k_channel_0"]['eigenvalues']),tot_number) for i in range(n_samples)])
 
     return eigenvalues
 
 def band_label_list(band_path_dict):
-    return [band_path_dict['label_start'] , band_path_dict['label_end']]
+    return [band_path_dict['label_start'], band_path_dict['label_end']]
 
 def band_parse(band_path_dict):
     return band_coordinates_list(band_path_dict), band_eigenvalues_list(band_path_dict), band_label_list(band_path_dict)
@@ -41,10 +42,10 @@ def named_structure_to_full_bands(named_structure_dict):
     
     return band.full_bands([band_to_bands(named_structure_dict['band_path_' + str(n)]) for n in range(n_band_paths)])
 
-def file_full_extract(json_dir):
-    with open(json_dir,'r') as f:
+def file_full_extract(json_dir, keyword):
+    with open(json_dir, 'r') as f:
         temp = f.read()
-        inp = json.loads(temp)
-        namelist = [i for i in inp]
+        inp = json.loads(temp.replace("'", "\"").replace("True", "true").replace("False", "false"))
 
-        return namelist,list(map(named_structure_to_full_bands,[inp[i] for i in inp]))
+        return named_structure_to_full_bands(inp[keyword])
+
